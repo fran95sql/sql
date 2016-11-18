d46ab778cb919911d3d37ff217bbeb46a68be778
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2016-11-18 14:51:21 +0100

    Add files via upload
    Añadida la creación de usuario y la gestión de permisos, falta por hacer la asignación de la BD en el Grant

---
+++ creacionusuario.sql
@@ -0,0 +1,12 @@
+/*  Paso 1: definimos el inicio de sesión: 
+use master
+create login ejemplo
+with password = '1234';*/
+
+Creación del usuario:
+create user Juan for login ejemplo;
+alter user Juan for login ejemplo
+with password = '1234';
+/* Se genera asigna por defecto el permiso Public, pero no nos permite conectar
+luego le asignamos permisos para conectarse */
+grant connect to Juan (seleccionar bd);
