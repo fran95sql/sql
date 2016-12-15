35ee940d034b27d82bad594d15587ca5af4d18ae
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2016-12-15 13:42:08 +0100

    Añadidos los BEGIN y END para control

--- TSQL/SQLQuery3.sql
+++ TSQL/SQLQuery3.sql
@@ -1,5 +1,8 @@
 DECLARE
 		@edad real;
 		SET @edad = 25;
-			IF (@edad < 30) AND (@edad >= 15)
-				PRINT 'JOVENES';
+			IF (@edad < 30) AND (@edad >= 25)
+				BEGIN
+					PRINT 'JOVENES'
+					PRINT 'QUE BIEN';
+				END
