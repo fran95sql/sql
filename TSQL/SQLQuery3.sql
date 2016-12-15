4dd2a1b7f45dd9758590ca8187285c82cabb9bb8
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2016-12-15 13:52:49 +0100

    Update SQLQuery3.sql

--- TSQL/SQLQuery3.sql
+++ TSQL/SQLQuery3.sql
@@ -1,8 +1,22 @@
-DECLARE
+/* DECLARE
 		@edad real;
 		SET @edad = 25;
 			IF (@edad < 30) AND (@edad >= 25)
 				BEGIN
 					PRINT 'JOVENES'
 					PRINT 'QUE BIEN';
+				END */
+	/*SELECCION DOBLE*/
+/*DECLARE
+		@edad real;
+		SET @edad = 100;
+			IF (@edad < 30) AND (@edad >= 25)
+				BEGIN
+					PRINT 'JOVENES'
+					PRINT 'QUE BIEN';
 				END
+			ELSE
+				BEGIN
+					PRINT 'VIEJOS'
+				END*/
+		/*SELECCION MULTIPLE*/
