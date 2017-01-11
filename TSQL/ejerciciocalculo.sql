e6362506e945cf7fc604969f4e727817728572be
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-01-11 23:57:01 +0100

    Arreglado el fallo de concepto con CASE
    /*Condicion si la suma es menor de 50 hacer raiz cuadrada de los cuadrados, si */
    DECLARE
    	@num smallint;
    	SET @num = 30;
    DECLARE 
    	@num2 smallint;
    	SET @num2 = 10;
    DECLARE
    	@num3 smallint;
    	SET @num3 = 10;
    DECLARE 
    	@raiz decimal (4,3);
    DECLARE
    	@suma int;
    DECLARE
    	@cuadrado int;
    DECLARE
    	@div float;
    BEGIN
    	SET @suma = @num+@num2+@num3;
    	IF (@suma < 50)
    		BEGIN
    			SET @raiz = sqrt(@num^2+@num2^2+@num3^2);
    			print @raiz;
    		END
    	IF (@suma = 50)
    		BEGIN
    			SET @cuadrado = @num^2+@num2^2+@num3^2;
    			print @cuadrado;
    		END
    	IF (@suma > 50)
    		BEGIN
    			SET @div = @num/(@num2*@num3);
    			print @div;
    		END
    END
     Era el original

--- TSQL/ejerciciocalculo.sql
+++ TSQL/ejerciciocalculo.sql
@@ -15,58 +15,17 @@ BEGIN
 	print @media;
 END
 
+DECLARE @num smallint, @num2 smallint, @num3 smallint;
 
-/*Los tipos de datos decimal y int son incompatibles con el operador '^'.*/
-/*Raíz cuadrada de sus cuiadrados*/
-DECLARE
-	@num smallint;
-	SET @num = 4;
-DECLARE 
-	@num2 smallint;
-	SET @num2 = 5;
-DECLARE
-	@num3 smallint;
-	SET @num3 = 8;
-DECLARE 
-	@raiz decimal (4,3);
-BEGIN
-	SET @raiz = sqrt(@num^2+@num^2+@num3^2);
-	print @raiz;
-END
+SET @num = 30;
+SET @num2 = 10;
+SET @num3 = 10;
 
-/*Condicion si la suma es menor de 50 hacer raiz cuadrada de los cuadrados, si */
-DECLARE
-	@num smallint;
-	SET @num = 30;
-DECLARE 
-	@num2 smallint;
-	SET @num2 = 10;
-DECLARE
-	@num3 smallint;
-	SET @num3 = 10;
-DECLARE 
-	@raiz decimal (4,3);
-DECLARE
-	@suma int;
-DECLARE
-	@cuadrado int;
-DECLARE
-	@div float;
-BEGIN
-	SET @suma = @num+@num2+@num3;
-	IF (@suma < 50)
-		BEGIN
-			SET @raiz = sqrt(@num^2+@num2^2+@num3^2);
-			print @raiz;
-		END
-	IF (@suma = 50)
-		BEGIN
-			SET @cuadrado = @num^2+@num2^2+@num3^2;
-			print @cuadrado;
-		END
-	IF (@suma > 50)
-		BEGIN
-			SET @div = @num/(@num2*@num3);
-			print @div;
-		END
-END
+DECLARE @suma int;
+SET @suma = @num+@num2+@num3;
+
+SELECT  CASE 
+            WHEN @suma < 50 THEN sqrt(@num^2+@num2^2+@num3^2)
+            WHEN @suma = 50 THEN @num^2+@num2^2+@num3^2
+            WHEN @suma > 50 THEN CAST(@num as float)/(@num2*@num3)
+        END
