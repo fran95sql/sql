46f0d4d37ec2feff4ad6f3e28d507a5eed87298b
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-01-13 11:08:44 +0100

    Ejercicio corregido

--- TSQL/ejercicoexpli.sql
+++ TSQL/ejercicoexpli.sql
@@ -1,12 +1,11 @@
 DECLARE
-	@dni varchar, @nombre varchar, @curso tinyint;
-DECLARE
+	@dni varchar, @nombre varchar, @curso tinyint, 
 	ras2 CURSOR FOR SELECT* FROM boi;
-OPEN ras2; /*RESERVAMOS EL ESPACIO EN MEMORIA*/
+OPEN ras2 FETCH ras2 INTO @dni, @nombre, @curso; /*RESERVAMOS EL ESPACIO EN MEMORIA*/
 WHILE @@FETCH_STATUS=0
   FETCH  ras2 INTO @dni,@nombre,@curso;
+  print @dni;
+  print @nombre;
+  print @curso;
 CLOSE ras2;
-print @dni;
-print @nombre;
-print @curso;
 DEALLOCATE ras2;/*LIBERAMOS EL ESPACIO EN MEMORIA*/
