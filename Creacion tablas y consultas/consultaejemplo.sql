8ddcc9e8eeca94d6b3bd81d5d5fe5890f44ab5c8
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-01-20 09:15:57 +0100

    Update consultaejemplo.sql

--- Creacion tablas y consultas/consultaejemplo.sql
+++ Creacion tablas y consultas/consultaejemplo.sql
@@ -3,3 +3,7 @@ select nombre
 from Empleado inner join Jefe on (Empleado.e# = Jefe.e#)
 order by nombre ASC
 ;
+/*Obtenemos el nombre de todos los empleados que tienen un sueldo superior al salario mínimo*/
+select nombre
+from Empleado
+where (salario >  707.60);
