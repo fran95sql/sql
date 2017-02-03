63a5bc96fc184233d5d0de390ad83230c74d3078
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-02-03 11:20:17 +0100

    Update triggersejercicio.sql

--- TSQL/triggersejercicio.sql
+++ TSQL/triggersejercicio.sql
@@ -1,3 +1,4 @@
+/*Evitamos que hayan más de 3 empleados*/
 CREATE TRIGGER limitaempleado
 ON Empleado
 FOR INSERT
@@ -9,3 +10,25 @@ IF ((select count(e#) from Empleado) > 3)
 	END
 ELSE
 	print 'El empleado ha sido registrado';
+/**/
+CREATE TRIGGER salario
+ON Empleado
+FOR UPDATE
+AS
+	BEGIN
+		DECLARE C1 CURSOR FOR (select salario from Empleado);
+		DECLARE @salmedio float, @salario float;
+		SET @salmedio = (select avg(salario) from Empleado);
+		OPEN C1
+		FETCH C1 into @salario;
+		WHILE @@FETCH_STATUS = 0
+			BEGIN
+				IF (@salario < @salario)
+					BEGIN
+					print('No cumple la restricción');
+					END
+			FETCH C1 into @salario;
+			END
+		CLOSE C1;
+		DEALLOCATE C1;
+	END
