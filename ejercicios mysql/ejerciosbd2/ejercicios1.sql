a1d49b22f6716a98749757c1dd83663f2d7c147c
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-01-30 09:28:24 +0100

    Update ejercicios1.sql

--- ejercicios mysql/ejerciosbd2/ejercicios1.sql
+++ ejercicios mysql/ejerciosbd2/ejercicios1.sql
@@ -4,3 +4,17 @@ mysqldump -u root -p alumnado /*Es la bd*/ > /home/rafa/Documentos/bdalumnado.sq
 -- importamos desde bash
 mysql -u root -p alumnado < alumnado /*Falla si existe antes, el script debe tener drop datatabase..*/
 /*mysqldump --databases --add-drop-database -u root -p  alumnado > /home/rafa/alumnado.sql*/
+
+/*Pruebas con mysql slap*/
+select edades.edad from nombres,edades 
+  where nombres.id = edades.id and nomnres.name ='Guillermo
+    order by edades.edad;
+ -- Desde bash
+ mysqlslap -v/*Verbose*/ -u root -p --auto-generate-sql
+ mysqlslap -u root --p --concurrency=60 --auto-generate--sql
+ mysqlslap -u root --p --concurrency=60 > --iterations = 5 --auto-generate--sql
+ mysqlslap -u root p --concurrency=60 > --iterations = 5 --number-of-queries = 200 --autogenerate-sql
+msqlsla -v root -p --delimiter ";" --create= "create table t (i int); insert into t values (11);" --query="select * from t"
+  --concurency=60 --iterations= 100
+  
+mysqlslap -u root -p --concurrency=5 --iteration= 100 --query=query.sql/*Consulta*/ --create=create.sqñ /*crea tablas*/
