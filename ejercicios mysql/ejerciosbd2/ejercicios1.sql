4f0b2ea46c82fbc65ec20a7d065b7cdc15e84698
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-01-30 09:07:10 +0100

    Update ejercicios1.sql

--- ejercicios mysql/ejerciosbd2/ejercicios1.sql
+++ ejercicios mysql/ejerciosbd2/ejercicios1.sql
@@ -1,1 +1,6 @@
-
+-- exportamos desde bash
+mysqldump -u root -p alumnado /*Es la bd*/ > /home/rafa/Documentos/bdalumnado.sql
+/*mysqldump -u root -p --databases alummnado test > /home/rafa/Documentos/bdalumnado.sql*/
+-- importamos desde bash
+mysql -u root -p alumnado < alumnado /*Falla si existe antes, el script debe tener drop datatabase..*/
+/*mysqldump --databases --add-drop-database -u root -p  alumnado > /home/rafa/alumnado.sql*/
